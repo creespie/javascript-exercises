5a2f4883c25710d2b99a3c06a2da27ff1f36b6a6
Ludovico rossi <ludovico@MBP-di-Ludovico.wind3.hub>
2024-07-22 14:33:10 +0200

    complete exercises 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,45 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+let total = 0;
+arr.map(number => total = total +number);
+return total;
 	
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let total = 1;
+  arr.map(number => total = total *number);
+  return total;
 };
 
-const multiply = function() {
-
+const power = function(number, power) {
+	let total = 1;
+  while(power >0){
+    total = total * number;
+    power--;
+  }
+  return total;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(number) {
+  total = 1;
+  if(number == 0){
+    return 1;
+  }else{
+    while(number > 0){
+      total = total * number;
+      number--;
+    };
+    return total;
+  };
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordFix = word.toLowerCase().replace(/[^a-z0-9]/g,"");
+
+    
+
+    let finalWord = wordFix.split("").reverse().join("");
+
+   return finalWord == wordFix;
 
 };
 
